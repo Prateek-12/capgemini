f8294737b66ed55640b59e3c500333905e803bc7
Prateek-12 <65523775+Prateek-12@users.noreply.github.com>
2022-03-27 23:44:31 +0530

    Delete JAVASCRIPT ASSIGNMENT directory

--- JAVASCRIPT ASSIGNMENT/Assignment1Q11/test.js
+++
@@ -1,7 +0,0 @@
-var myArray = ["Captain America",54,"Black Widow",63,true,5.23,"tom and jerry",'A',"C"];
-function reverseArray(a){
-    a.reverse();
-    return a;
-}
-
-document.getElementById("arr").innerHTML = reverseArray(myArray);
